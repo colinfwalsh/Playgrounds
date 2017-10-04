b150dc67ebf3613b38ce75b581f48b0b551a8a36
Colin Walsh <colinfwalsh@gmail.com>
2017-10-03 23:40:43 -0400

    BT finished

--- swiftDataStructures.playground/Contents.swift
+++ swiftDataStructures.playground/Contents.swift
@@ -52,6 +52,7 @@ final class LinkedList<T> {
 
 //Binary Search Tree - from RayWanderlich
 
+//Binary Tree implementation
 class Node<T> {
     var value: T
     var leftChild: Node?
@@ -62,4 +63,58 @@ class Node<T> {
     }
 }
 
+//indirect allows value types to be interpreted as reference types so recursive calls can take place
+enum BinaryTree<T> {
+    case empty
+    indirect case node(BinaryTree, T, BinaryTree)
+    
+    var count: Int {
+        switch self {
+        case let .node(left, _, right):
+            return left.count + 1 + right.count
+        case .empty:
+            return 0
+        }
+    }
+}
+
+//Specify custom output
+extension BinaryTree: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case let .node(left, value, right):
+            return
+                "value: \(value), left = [" + left.description +
+                "], right = [" + right.description + "]"
+        case .empty:
+            return ""
+        }
+    }
+}
+
+//leafs
+let node5 = BinaryTree.node(.empty, "5", .empty)
+let nodeA = BinaryTree.node(.empty, "a", .empty)
+let node10 = BinaryTree.node(.empty, "10", .empty)
+let node4 = BinaryTree.node(.empty, "4", .empty)
+let node3 = BinaryTree.node(.empty, "3", .empty)
+let nodeB = BinaryTree.node(.empty, "b", .empty)
+
+//setting left tree
+    //right subtree in left tree
+    let aMinus10 = BinaryTree.node(nodeA, "-", node10)
+//set root left tree, right is aforementioned tree
+let timesLeft = BinaryTree.node(node5, "*", aMinus10)
+
+//setting right tree
+let minus4 = BinaryTree.node(.empty, "-", node4)
+    //right subtree in right tree
+    let divide3andB = BinaryTree.node(node3, "/", nodeB)
+//root node in right subtree
+let timesRight = BinaryTree.node(minus4, "*", divide3andB)
+
+//root node
+let tree = BinaryTree.node(timesLeft, "+", timesRight)
+
+print(tree.count)
 
